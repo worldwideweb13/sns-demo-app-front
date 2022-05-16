d2c6440870aa364130896a458d346c33e166510a
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-05-17 06:29:22 +0900

    :wrench: material-UI追加

--- src/components/topbar/Topbar.tsx
+++ src/components/topbar/Topbar.tsx
@@ -1,3 +1,4 @@
+import { Chat, Notifications, Search } from "@mui/icons-material";
 import React from "react";
 
 const Home: React.FC = () => {
@@ -8,6 +9,7 @@ const Home: React.FC = () => {
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
+          <Search className="searchIcon" />
           <input
             type="text"
             name="searchInput"
@@ -16,8 +18,14 @@ const Home: React.FC = () => {
         </div>
       </div>
       <div className="topbarRight">
-        <div className="topbarIconItem">1</div>
-        <div className="topbarIconItem">2</div>
+        <div className="topbarIconItem">
+          <Chat />
+          <span className="topbarIconItem">1</span>
+        </div>
+        <div className="topbarIconItem">
+          <Notifications />
+          <span className="topbarIconItem">2</span>
+        </div>
       </div>
       <img
         src="../../../public/assets/person/1.jpeg"
