250f2e266102f2ce26f08599a23bebc1fe49ffc2
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-05-17 18:38:11 +0900

    :art: cssの加筆修正 #3

--- src/components/topbar/Topbar.tsx
+++ src/components/topbar/Topbar.tsx
@@ -1,37 +1,37 @@
 import { Chat, Notifications, Search } from "@mui/icons-material";
 import React from "react";
+import "./Topbar.css";
+import profileImg from "../../assets/person/1.jpeg";
 
 const Home: React.FC = () => {
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
-        <span className="logo"></span>
+        <span className="logo">Real SNS</span>
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
           <Search className="searchIcon" />
           <input
             type="text"
-            name="searchInput"
+            className="searchInput"
             placeholder="探し物はなんですか？"
           />
         </div>
       </div>
       <div className="topbarRight">
-        <div className="topbarIconItem">
-          <Chat />
-          <span className="topbarIconItem">1</span>
-        </div>
-        <div className="topbarIconItem">
-          <Notifications />
-          <span className="topbarIconItem">2</span>
+        <div className="topbarItemIcons">
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <img src={profileImg} alt="" className="topbarImg" />
         </div>
       </div>
-      <img
-        src="../../../public/assets/person/1.jpeg"
-        alt=""
-        className="topbarImg"
-      />
     </div>
   );
 };
